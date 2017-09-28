2f97e852e244e5b52062dcfcd37980f618bd4e15
jais210 <jackelineisabel.puruaya@gmail.com>
2017-09-28 05:54:57 -0500

    js html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD:assets/js/main.js
 'use strict'
-const busMapa = function() {
-        let cantidadAsiento = 40;
+const distribucionAsientos = function() {
+        let cantidadAsiento = 20;
         let columnas = 4;
         let filas = cantidadAsiento / columnas;
         let asientosHTML = "",
@@ -10,9 +9,9 @@ const busMapa = function() {
             asientosHTML += `<div class='row' id='fila${i}'>`;
 
             for (let j = 0; j < columnas; j++) {
-                asientosHTML += `<div class='col col-xl-2' id='${numeroDeAsiento}'>${numeroDeAsiento}</div>`;
+                asientosHTML += `<div class='col col-xl-2 text-center' id='${numeroDeAsiento}'>${numeroDeAsiento}</div>`;
                 numeroDeAsiento += 1;
-                (j == 1) ? asientosHTML += "<div class='col col-xl-1'></div>": asientosHTML += "";
+                (j == 1) ? asientosHTML += "<div class='col col-xl-1 text-center'></div>": asientosHTML += "";
 
             }
             asientosHTML += "</div>";
@@ -20,31 +19,31 @@ const busMapa = function() {
         }
         return asientosHTML;
     }
-    /** ------- -----  */
+    /**/
 
 const reserva = {
     pasajeros: [{
             nroAsiento: "39",
-            nombre: "Maritza Fernandes",
+            nombre: "Mauricio Gamarra",
             dni: "89898998",
             estado: true
         },
         {
             nroAsiento: "3",
-            nombre: "Carolina Baez",
+            nombre: "Carmen Gonzales",
             dni: "89236723",
             estado: true
         },
         {
             nroAsiento: "15",
-            nombre: "Elizabeth Mamani",
+            nombre: "Elizabeth Monroy",
             dni: "78723211",
             estado: true
         }
     ],
     dniBuscado: undefined,
     inicio: () => {
-        $('#bus').html(busMapa);
+        $('#bus').html(distribucionAsientos);
         reserva.colorearAsientos();
         $('.col-xl-2').click(reserva.reservarAsiento);
         $('#guardarDatos').click(reserva.guardarDatos);
@@ -88,9 +87,9 @@ const reserva = {
         let lista = "";
         reserva.pasajeros.map((elemento) => {
             lista += `<div class='row'>\
-            <div class='col col-xl-2 col-sm-2'>${elemento.nroAsiento}</div>\
-            <div class='col col-xl-5 col-sm-5'>${elemento.nombre}</div>\
-            <div class='col col-xl-2 col-sm-2'>${elemento.dni}</div>\
+            <div class='col col-xl-2 col-sm-2 text-center'>${elemento.nroAsiento}</div>\
+            <div class='col col-xl-5 col-sm-5 text-center'>${elemento.nombre}</div>\
+            <div class='col col-xl-2 col-sm-2 text-center'>${elemento.dni}</div>\
             </div>`;
         });
         $('#listaPasajeros').html(lista)
@@ -135,200 +134,3 @@ const reserva = {
     }
 }
 $(document).ready(reserva.inicio);
-=======
-'use strict';
-/// asientos
-class Bus {
-    constructor() {
-        this.pasajeros = [];
-    }
-    agregarPasajero(nombre, dni, numeroAsiento) {
-        let pasajero = {
-            nombre: nombre,
-            dni: dni,
-            numeroAsiento: numeroAsiento,
-        }
-        this.pasajeros.push(pasajero);
-        return pasajero;
-
-    }
-    mostrar(pasajero) {
-        let fichaPasajero =
-            `
-            <div class = "pasajero">
-                <h3 class="text-uppercase">${pasajero.nombre}</h3>
-                <strong>Tech Skills:</strong> ${pasajero.dni}%<br>
-                <strong>Life Skills:</strong> ${pasajero.numeroAsiento}%<br>
-                <strong>Status:</strong> Active<br>
-            </div>
-        `
-        return fichaPasajero;
-    }
-    mostrarListaPasajero(pasajeros) {
-        return pasajeros.map(this.mostrar);
-    }
-}
-}
-
-// funciones 
->>>>>>> refs/remotes/origin/master:main.js
-
-function buscar(asientos, dni) {
-    for (var i = 0; i < asientos.length; i++) {
-        if (asientos[i] != undefined)
-            if (asientos[i].dni == dni)
-                return asientos[i];
-    }
-    return '';
-}
-
-function mostrar(asientos) {
-    var s1 = "",
-        s2 = "";
-    for (var i = 0; i < asientos.length; i++) {
-        var e = (asientos[i] != undefined) ? '*' : '';
-        if (i % 2 == 0)
-            s1 += (i + 1) + "[" + e + "] ";
-        else
-            s2 += (i + 1) + "[" + e + "] ";
-    }
-    return "\n" + s1 + "\n" + s2 + "\n";
-}
-
-function reserva() {
-    var N = 10; // Número de asientos
-    var asientos = [];
-    for (var i = 0; i < N; i++) {
-        asientos[i] = undefined;
-    }
-    var mensaje = "0: Salir\n" +
-        "1: Reservar  asiento\n" +
-        "2: Liberar asiento \n" +
-        "3: Seleccionar asiento \n" +
-        "4: Buscar por DNI \n";
-
-    var option = 0;
-    while (true) {
-        var str = mostrar(asientos) + mensaje + " >> ingrese opcion:";
-        option = parseInt(prompt(str));
-        if (option == 0) {
-            break;
-        } else if (option == 1) {
-            str = "seleccione asiento: " + mostrar(asientos);
-            var nro = parseInt(prompt(str));
-            if (nro > 0 && nro <= N) {
-                var name = prompt("nombre del pasajero");
-                var id = parseInt(prompt("dni del pasajero"));
-                asientos[nro - 1] = {
-                    nombre: name,
-                    dni: id
-                };
-            }
-        } else if (option == 2) {
-            str = "seleccione asiento: " + mostrar(asientos);
-            var nro = parseInt(prompt(str));
-            if (nro > 0 && nro <= N) {
-                asientos[nro - 1] = undefined;
-            }
-        } else if (option == 3) {
-            str = "seleccione asiento: " + mostrar(asientos);
-            var nro = parseInt(prompt(str));
-            if (nro > 0 && nro <= N) {
-                str = mostrar(asientos) +
-                    "Nombre del pasajero: " + asientos[nro - 1].nombre +
-                    "DNI del pasajero: " + asientos[nro - 1].dni;
-
-                alert(str);
-            }
-        } else if (option == 4) {
-            str = "ingrese dni: ";
-            var dni = parseInt(prompt(str));
-
-            alert("usuario:" + buscar(asientos, dni).nombre);
-        }
-
-    }
-    imprimir(asientos);
-}
-
-reserva();
-
-function buscar(asientos, dni) {
-    for (var i = 0; i < asientos.length; i++) {
-        if (asientos[i] != undefined)
-            if (asientos[i].dni == dni)
-                return asientos[i];
-    }
-    return '';
-}
-
-function mostrar(asientos) {
-    var s1 = "",
-        s2 = "";
-    for (var i = 0; i < asientos.length; i++) {
-        var e = (asientos[i] != undefined) ? '*' : '';
-        if (i % 2 == 0)
-            s1 += (i + 1) + "[" + e + "] ";
-        else
-            s2 += (i + 1) + "[" + e + "] ";
-    }
-    return "\n" + s1 + "\n" + s2 + "\n";
-}
-
-function reserva() {
-    var N = 10; // Número de asientos
-    var asientos = [];
-    for (var i = 0; i < N; i++) {
-        asientos[i] = undefined;
-    }
-    var mensaje = "0: Salir\n" +
-        "1: Reservar  asiento\n" +
-        "2: Liberar asiento \n" +
-        "3: Seleccionar asiento \n" +
-        "4: Buscar por DNI \n";
-
-    var option = 0;
-    while (true) {
-        var str = mostrar(asientos) + mensaje + " >> ingrese opcion:";
-        option = parseInt(prompt(str));
-        if (option == 0) {
-            break;
-        } else if (option == 1) {
-            str = "seleccione asiento: " + mostrar(asientos);
-            var nro = parseInt(prompt(str));
-            if (nro > 0 && nro <= N) {
-                var name = prompt("nombre del pasajero");
-                var id = parseInt(prompt("dni del pasajero"));
-                asientos[nro - 1] = {
-                    nombre: name,
-                    dni: id
-                };
-            }
-        } else if (option == 2) {
-            str = "seleccione asiento: " + mostrar(asientos);
-            var nro = parseInt(prompt(str));
-            if (nro > 0 && nro <= N) {
-                asientos[nro - 1] = undefined;
-            }
-        } else if (option == 3) {
-            str = "seleccione asiento: " + mostrar(asientos);
-            var nro = parseInt(prompt(str));
-            if (nro > 0 && nro <= N) {
-                str = mostrar(asientos) +
-                    "Nombre del pasajero: " + asientos[nro - 1].nombre +
-                    "DNI del pasajero: " + asientos[nro - 1].dni;
-
-                alert(str);
-            }
-        } else if (option == 4) {
-            str = "ingrese dni: ";
-            var dni = parseInt(prompt(str));
-
-            alert("usuario:" + buscar(asientos, dni).nombre);
-        }
-
-    }
-    imprimir(asientos);
-}
-
-reserva();
