494c86ed5f9d1ab175a42a2984c0e718a2f914df
jais210 <jackelineisabel.puruaya@gmail.com>
2017-09-28 07:53:30 -0500

    html js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 'use strict'
 const reserva = {
     pasajeros: [{
+        // array de pasajeros
             nroAsiento: "2",
             nombre: "Mauricio Gamarra",
             dni: "89898998",
@@ -10,13 +11,34 @@ const reserva = {
     ],
     bucarDni: undefined,
     inicio: () => {
-        $('#bus').html(distribucionAsientos);
+        
+        $('#bus').html(reserva.distribucionAsientos);
         reserva.colorearAsientos();
         $('.col-xl-2').click(reserva.reservarAsiento);
         $('#guardarDatos').click(reserva.guardarDatos);
         $('#mostrarLista').click(reserva.mostrarLista);
         $('#btnBuscar').click(reserva.buscarDNI);
         $('#eliminarReserva').click(reserva.eliminarReserva);
+    },
+    distribucionAsientos:()=> {
+        let totalAsientos = 20;
+        let columnas = 4;
+        let filas = totalAsientos / columnas;
+        let html = "",
+            numeroAsiento = 1;
+        for (let i = 1; i <= filas; i++) {
+            html += `<div class='row' id='fila${i}'>`;
+    
+            for (let j = 0; j < columnas; j++) {
+                html += `<div class='col col-xl-2 text-center' id='${numeroAsiento}'>${numeroAsiento}</div>`;
+                numeroAsiento += 1;
+                (j == 1) ? html += "<div class='col col-xl-1 text-center'></div>": html += "";
+    
+            }
+            html += "</div>";
+    
+        }
+        return html;
     },
     reservarAsiento: (event) => {
         let nro = event.target.textContent;
@@ -67,10 +89,10 @@ const reserva = {
             return elemento.dni == dni;
         });
         if (reserva.bucarDni.length != 0) {
-            $('#listaPasajeros').html(`<div class='row'>\
-            <div class='col col-xl-2 col-sm-2 '>${reserva.bucarDni[0].nroAsiento}</div>\
-            <div class='col col-xl-5 col-sm-5'>${reserva.bucarDni[0].nombre}</div>\
-            <div class='col col-xl-2 col-sm-3'>${reserva.bucarDni[0].dni}</div>\
+            $('#listaPasajeros').html(`<div id="tablaPasajero" class='row'>\
+            <div id="tablaPasajero" class='col col-xl-2 col-sm-2 text-center'>${reserva.bucarDni[0].nroAsiento}</div>\
+            <div id="tablaPasajero" class='col col-xl-5 col-sm-5 text-center'>${reserva.bucarDni[0].nombre}</div>\
+            <div id="tablaPasajero" class='col col-xl-2 col-sm-3 text-center'>${reserva.bucarDni[0].dni}</div>\
             </div>`);
         } else {
             $('#listaPasajeros').html(`No hay Resultados`);
@@ -99,25 +121,7 @@ const reserva = {
             return $('#' + elemento.nroAsiento).addClass('reservado');
         });
     }
+   
 }
-const distribucionAsientos = function() {
-    let totalAsientos = 20;
-    let columnas = 4;
-    let filas = totalAsientos / columnas;
-    let html = "",
-        numeroAsiento = 1;
-    for (let i = 1; i <= filas; i++) {
-        html += `<div class='row' id='fila${i}'>`;
 
-        for (let j = 0; j < columnas; j++) {
-            html += `<div class='col col-xl-2 text-center' id='${numeroAsiento}'>${numeroAsiento}</div>`;
-            numeroAsiento += 1;
-            (j == 1) ? html += "<div class='col col-xl-1 text-center'></div>": html += "";
-
-        }
-        html += "</div>";
-
-    }
-    return html;
-}
 $(document).ready(reserva.inicio);
